5e98233e0477322b806b6152fc30b93527483c30
michael760odhiambo <michaelodhiambo760@gmail.com>
2019-08-19 12:15:22 +0300

    save mikes added quote

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,7 +9,7 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes: Quote[] = [
-  new Quote(1,'Love quotes','1)A friend is someone who knows all about you and still loves you, 2) Darkness cannot drive out darkness only light can do that Hate cannot drive out hate only love can do that,3)We accept the love we think we deserve', new Date(2019,6,9)),
+  new Quote(1,'Love quotes : mike','1)A friend is someone who knows all about you and still loves you, 2) Darkness cannot drive out darkness only light can do that Hate cannot drive out hate only love can do that,3)We accept the love we think we deserve', new Date(2019,6,9)),
   new Quote(2,'Motivational quotes','1)Successful people don’t become that way overnight, 2)Push yourself, because no one else is going to do it for you, 3)Why pushing yourself to the limit is the best way to see what you’re capable of, 4) Dream it. Wish it. Do it.', new Date(2019,6,9)),
    new Quote(3,'Inspirational quotes','1)Dont judge each day by the harvest you reap but by the seeds that you plant, 2)Write it on your heart that every day is the best day in the year, 3)Every moment is a fresh beginning, 4)Without His love I can do nothing, with His love there is nothing I cannot do', new Date(2019,6,9)),
    new Quote(4,'Dream quotes','1)A dream doesnt become reality through magic; it takes sweat, determination and hard work, 2)All our dreams can come true, if we have the courage to pursue them, 3)Hold fast to dreams, for if dreams die, life is a broken-winged bird that cannot fly, 4)The future belongs to those who believe in the beauty of their dreams', new Date(2019,6,9)),
